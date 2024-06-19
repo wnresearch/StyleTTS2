dbfcbeedb26550356dbeae9f9ea3dc0b606c3cca
Liane Nakamura <lianenakamura@gmail.com>
2024-06-18 17:54:48 -0700

    ESpeakPhonemizer

--- src/styletts2/phoneme.py
+++ src/styletts2/phoneme.py
@@ -1,34 +1,29 @@
-from gruut import sentences
 from collections.abc import Iterable
+import phonemizer
+from nltk.tokenize import word_tokenize
 
+espeak_phonemizer = phonemizer.backend.EspeakBackend(language='en-us', preserve_punctuation=True,  with_stress=True)
 
 class PhonemeConverter:
     def phonemize(self, text):
         pass
 
 
-class GruutPhonemizer(PhonemeConverter):
+class ESpeakPhonemizer(PhonemeConverter):
     def phonemize(self, text, lang='en-us'):
-        phonemized = []
-        for sent in sentences(text, lang=lang):
-            for word in sent:
-                if isinstance(word.phonemes, Iterable):
-                    phonemized.append(''.join(word.phonemes))
-                elif isinstance(word.phonemes, str):
-                    phonemized.append(word.phonemes)
-        phonemized_text = ' '.join(phonemized)
-        return phonemized_text
+        text = text.strip()
+        text = text.replace('"', '')
+        ps = espeak_phonemizer.phonemize([text])
+        ps = word_tokenize(ps[0])
+        ps = ' '.join(ps)
 
-
-# class YourPhonemizer(Phonemizer):
-#     def phonemize(self, text):
-#         ...
+        return ps
 
 
 class PhonemeConverterFactory:
     @staticmethod
     def load_phoneme_converter(name: str, **kwargs):
-        if name == 'gruut':
-            return GruutPhonemizer()
+        if name == 'espeak':
+            return ESpeakPhonemizer()
         else:
             raise ValueError("Invalid phoneme converter.")
--- src/styletts2/tts.py
+++ src/styletts2/tts.py
@@ -20,6 +20,7 @@ np.random.seed(0)
 
 from txtsplit import txtsplit
 import yaml
+import re
 
 from . import models
 from . import utils
@@ -66,7 +67,7 @@ def segment_text(text):
 
 
 class StyleTTS2:
-    def __init__(self, model_checkpoint_path=None, config_path=None, phoneme_converter='gruut'):
+    def __init__(self, model_checkpoint_path=None, config_path=None, phoneme_converter='espeak'):
         self.model = None
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.phoneme_converter = PhonemeConverterFactory.load_phoneme_converter(phoneme_converter)
